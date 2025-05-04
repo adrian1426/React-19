14b038ad9b854cd4aab8b222a4058998ed1258a7
adrianhh1426 <adrianhh1426@gmail.com>
2025-05-03 18:38:13 -0600

    metadata y default props

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { version } from 'react';
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Seo from './components/seo';
 
 function App() {
 
   return (
     <>
+      <title>`Hola React ${version}`</title>
       <div>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
         <p>version of React {version}</p>
+        <hr />
+        <Seo />
       </div>
     </>
   )
---
+++ src/components/seo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Seo = ({ title = 'default value' }) => {
+  return (
+    <div>
+      Mi title {title}
+    </div>
+  );
+};
+
+export default Seo;
